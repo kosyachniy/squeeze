bfebbe3865cff991cfb9e3dfee172c3c384b9bd5
Poloz Alexey <polozhev@mail.ru>
2017-04-02 23:51:06 +0300

    United splitting words and definition of categories. Optimization.

--- parse.py
+++ parse.py
@@ -5,21 +5,41 @@ from pymorphy2 import MorphAnalyzer
 import re
 import os
 signs=',.!?\'":;/&'
+m=MorphAnalyzer()
+
+def convert(item):
+	if item:
+		return item[0:4]
+	else:
+		return ''
+
+def morph(text):
+#	MongoDB
+	p=(m.parse(text)[0]).tag
+	return convert(p.POS),convert(p.gender),convert(p.case),convert(p.number)
+#	print(p)
+#import subprocess
+#	cmd='echo "'+i.cont+'" | '+os.getcwd()+'/mystem -i'
+#	PIPE=subprocess.PIPE
+#	p=subprocess.Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE,
+#	stderr=subprocess.STDOUT, close_fds=True, cwd='/home/')
+#	print(bytes(p.stdout.read()).decode('utf8'))
 
 def parse(str):
-#Разбиение текста на слова и объединение знаков
+#Разбиение текста на слова
 	str=re.sub(r'(['+signs+'()<>\[\]])',r' \1 ',str).split()
 	class word:
-		def __init__(self,cont='',speech=''):
+		def __init__(self,cont='',speech='',gender='',case='',number=''):
 			self.cont=cont
 			self.speech=speech
 			self.sentence=''
-			self.case=''
-			self.number=''
-			self.gender=''
+			self.case=case
+			self.number=number
+			self.gender=gender
 	text=[]
 	t=False
 	for i in range(len(str)):
+#Объединение знаков
 		if str[i] in signs:
 			if t:
 				text[len(text)-1].cont+=str[i]
@@ -27,27 +47,9 @@ def parse(str):
 				text.append(word(str[i],'signs'))
 				t=True
 		else:
-			text.append(word(str[i]))
-			t=False
-
 #Определение граммем (части речи, падежа, рода, числа, ...)
-#	MongoDB
-
-	morph=MorphAnalyzer()
-	for i in text:
-		if i.speech!='signs':
-			p=(morph.parse(i.cont)[0]).tag
-			i.speech=p.POS
-			i.gender=p.gender
-			i.case=p.case
-			i.number=p.number
-#			print(p)
-#import subprocess
-#			cmd='echo "'+i.cont+'" | '+os.getcwd()+'/mystem -i'
-#			PIPE=subprocess.PIPE
-#			p=subprocess.Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE,
-#			stderr=subprocess.STDOUT, close_fds=True, cwd='/home/')
-#			print(bytes(p.stdout.read()).decode('utf8'))
+			text.append(word(str[i],morph(str[i])[0],morph(str[i])[1],morph(str[i])[2],morph(str[i])[3]))
+			t=False
 
 #Объединение слов в предложения
 	class sentence:
@@ -58,7 +60,7 @@ def parse(str):
 	num=0
 	mas=[sentence()]
 	for i in range(len(text)):
-		if text[i].speech in ('PRED', 'NPRO', 'CONJ', 'PRCL', 'ADJF', 'NUMR', 'ADJS', 'NOUN', 'VERB', 'INTJ', 'PRTS', 'ADVB', 'PRTF', 'INFN', 'PREP', 'GRND', 'COMP'):
+		if text[i].speech!='signs':
 			mas[num].count+=1
 		mas[num].word.append({'original':text[i].cont,'change':text[i].cont,'numsp':num+1,'speech':text[i].speech,'sentence':text[i].sentence,'case':text[i].case,'number':text[i].number,'gender':text[i].gender})
 		if any(c in ('.!?') for c in text[i].cont) and (i!=len(text)-1):
