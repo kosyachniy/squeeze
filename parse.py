14a23209eb8f48caea57c344bf0301943c4afe7c
Poloz Alexey <polozhev@mail.ru>
2017-04-02 20:47:56 +0300

    Optimization

--- parse.py
+++ parse.py
@@ -2,54 +2,46 @@
 
 #from pymongo import MongoClient
 from pymorphy2 import MorphAnalyzer
-import subprocess
 import re
 import os
-allsigns=(',.!?\'":;()/&')
 
 def parse(str):
 #Разбиение на слова и объединение знаков
 	str=re.sub(r'([.,!?"/():;&\'])',r' \1 ',str).split()
 	class word:
-		cont=''
-		speech=''
-		sentence=''
-		case=''
-		number=''
-		gender=''
+		def __init__(self,cont=''):
+			self.cont=cont
+			self.speech=''
+			self.sentence=''
+			self.case=''
+			self.number=''
+			self.gender=''
 	text=[]
 	for i in range(len(str)):
-		text.append(word())
-		text[i].cont=str[i]
-		if text[i].cont in allsigns:
+		text.append(word(str[i]))
+		if text[i].cont in ',.!?\'":;()/&':
 			text[i].speech='signs'
 	i=0
 	while i<=len(text)-2:
-		if (text[i].cont in allsigns) and (text[i+1].cont in allsigns):
+		if (text[i].speech=='signs') and (text[i+1].speech=='signs'):
 			text[i].cont+=text[i+1].cont
 			del text[i+1]
-			if (i<=len(text)-2) and (text[i+1].cont in allsigns):
-				text[i].cont+=text[i+1].cont
-				del text[i+1]
-		i+=1
+		else:
+			i+=1
 
 #Определение граммем (части речи, падежа, рода, числа, ...)
-
-#	client=MongoClient()
-#	db=client['k']
-#	cursor=db.user.find()
-#	for doc in cursor:
-#		print(doc)
+#	MongoDB
 
 	morph=MorphAnalyzer()
 	for i in text:
 		if i.speech!='signs':
 			p=(morph.parse(i.cont)[0]).tag
-			print(p)
 			i.speech=p.POS
 			i.gender=p.gender
 			i.case=p.case
 			i.number=p.number
+#			print(p)
+#import subprocess
 #			cmd='echo "'+i.cont+'" | '+os.getcwd()+'/mystem -i'
 #			PIPE=subprocess.PIPE
 #			p=subprocess.Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE,
@@ -58,10 +50,10 @@ def parse(str):
 
 #Разбиение на предложения
 	class sentence:
-		def __init__(self):
+		def __init__(self,number=1):
 			self.word=[]
 			self.count=0
-		number=1
+			self.number=number
 	num=0
 	mas=[sentence()]
 	for i in range(len(text)):
@@ -70,8 +62,7 @@ def parse(str):
 		mas[num].word.append({'original':text[i].cont,'change':text[i].cont,'numsp':num+1,'speech':text[i].speech,'sentence':text[i].sentence,'case':text[i].case,'number':text[i].number,'gender':text[i].gender})
 		if any(c in ('.!?') for c in text[i].cont) and (i!=len(text)-1):
 			num+=1
-			mas.append(sentence())
-			mas[num].number=num+1
+			mas.append(sentence(num+1))
 
 #Определение членов предложения (ЗАМЕНИТЬ НЕЙРОНКОЙ)
 	t=False
@@ -85,7 +76,7 @@ def parse(str):
 			for j in i.word:
 				if j['speech'] in ('VERB','INFN'):
 					j['sentence']='predicate'
-	#Если предложение из одного слова???
+#	Если предложение из одного слова???
 	for i in mas:
 		if i.count==1:
 			for j in i.word:
