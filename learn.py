e5589fc2cf29b18f64d4f134fab440a369fbfedf
Poloz Alexey <polozhev@mail.ru>
2017-04-09 17:55:05 +0300

    The neural network training module has been added. Changed the output format.

--- learn.py
+++ learn.py
@@ -1,39 +1,19 @@
 from parse import parse
 
-opensigns='(<\[\{'+'«'
-
-def assembly(mas):
-	text=''
-	t=False
-	for i in mas:
-		for j in i.word:
-			if (j['infinitive'] in opensigns) and t:
-				text+=' '
-				t=False
-			elif (j['speech']!='sign') and t:
-				text+=' '
-			else:
-				t=True
-			text+=j['infinitive']
-	return text
-
 def learn():
 	f=open('db','r')
 	orig=[]
 	chang=[]
+	inp=[]
+	out=[]
 	t=True
 	for i in f:
 		if f:
+			inp.append(i[:-2])
 			orig.append(parse(i))
 			f=False
 		else:
+			out.append(i[:-2])
 			chang.append(parse(i))
 			f=True
-	return orig, chang
-
-a,b=learn()
-for i in a:
-	print(assembly(i))
-print('-----')
-for i in b:
-	print(assembly(i))
+	return orig, chang, inp, out
