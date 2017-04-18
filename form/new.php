bc64862b44f8348b53de43c0fd03ef766a5464ad
Poloz Alexey <polozhev@mail.ru>
2017-04-18 21:59:48 +0300

    The database of texts is replenished

--- form/new.php
+++ form/new.php
@@ -5,16 +5,27 @@ header('location: ./');
 $cont=text($_POST['cont']);
 $dop=text($_POST['dop']);
 
-$time=date('d.m.Y H:i:s');
-$user=$_SERVER['REMOTE_ADDR'];
-mysqli_query($db,"INSERT INTO `note`(`cont`,`dop`,`time`,`user`) VALUES ('$cont','$dop','$time','$user');");
+if ($cont)
+	{
+	$t=True;
+	$res=mysqli_query($db,"SELECT * FROM `note`");
+	while($row=mysqli_fetch_array($res))
+		if ($row['cont']==$cont)
+			$t=False;
+	if ($t)
+		{
+		$time=date('d.m.Y H:i:s');
+		$user=$_SERVER['REMOTE_ADDR'];
+		mysqli_query($db,"INSERT INTO `note`(`cont`,`dop`,`time`,`user`) VALUES ('$cont','$dop','$time','$user');");
 
-$in=fopen('input.txt','a');
-$out=fopen('output.txt','a');
-fwrite($in,$cont.'
-');
-fwrite($out,$dop.'
-');
-fclose($in);
-fclose($out);
+		$in=fopen('input.txt','a');
+		$out=fopen('output.txt','a');
+		fwrite($in,$cont.'
+		');
+		fwrite($out,$dop.'
+		');
+		fclose($in);
+		fclose($out);
+		}
+	}
 ?>
